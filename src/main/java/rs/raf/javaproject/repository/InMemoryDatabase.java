777dc0d45b35c158c876a1283eb39dc8da64aef2
Mihailo97 <mvignjevic97@gmail.com>
2020-06-01 14:55:51 +0200

    Milos hoce da cuka

--- src/main/java/rs/raf/javaproject/repository/InMemoryDatabase.java
+++ src/main/java/rs/raf/javaproject/repository/InMemoryDatabase.java
@@ -3,12 +3,26 @@ package rs.raf.javaproject.repository;
 import rs.raf.javaproject.model.IJob;
 import rs.raf.javaproject.model.INode;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class InMemoryDatabase {
 
-    ArrayList<INode> allNodes = new ArrayList<>();
-    ArrayList<IJob> allJobs = new ArrayList<>();
+    Collection<INode> allNodes;
+    Collection<IJob> allJobs;
 
+    private static InMemoryDatabase instance;
 
+    private InMemoryDatabase(){
+        allNodes = new ArrayList<>();
+        allJobs = new ArrayList<>();
+    }
+
+    public static InMemoryDatabase getInstance() {
+        if(instance == null){
+            instance = new InMemoryDatabase();
+        }
+        return instance;
+    }
 }
