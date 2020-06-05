8566ae858f85f60d9f1486e9a1a93935c92fdb8a
Mihailo97 <mvignjevic97@gmail.com>
2020-06-05 02:40:36 +0200

    DatabaseService 80%

--- src/main/java/org/example/service/DatabaseService.java
+++ src/main/java/org/example/service/DatabaseService.java
@@ -17,42 +17,87 @@ public class DatabaseService {
     Database database;
 
     public synchronized Collection<Node> getAllNodes(){
-        return null; // TODO
+        return database.getAllNodes();
     }
 
     public synchronized Collection<Job> getAllJobs(){
-        return null; // TODO
+        return database.getAllJobs(); // TODO
     }
 
+    // Vraca node za odredjeni id ili null ukoliko ne postoji
     public synchronized Node getNodeFromID(String nodeID){
-        return null; // TODO
+        for(Node node: database.getAllNodes()){
+            if(node.getID().equals(nodeID)){
+                return node;
+            }
+        }
+        return null;
     }
 
+    // Vraca job za odredjeni id ili null ukoliko ne postoji
     public synchronized Job getJobFromID(String jobID){
-        return null; // TODO
+
+        for(Job job: database.getAllJobs()){
+            if(job.getId().equals(jobID)){
+                return job;
+            }
+        }
+        return null;
     }
 
     public synchronized Collection<Node> getNodesForJob(Job job){
-        return null; // TODO
+        ArrayList<Node> nodes = new ArrayList<>();
+
+        for(Map.Entry<Node, String> entry: database.getCurrentWork().entrySet()){
+            String[] work = entry.getValue().split(":");
+            String jobID = work[0];
+            String regionID = work[1];
+
+            if(jobID.equals(job.getId())){
+                nodes.add(entry.getKey());
+            }
+        }
+
+        return nodes;
     }
 
     public synchronized Collection<Node> getNodesForJobID(String jobID){
-        return null; // TODO
+        return getNodesForJob(getJobFromID(jobID));
     }
 
     public synchronized  Collection<Node> getNodesForJobAndRegion(Job job, Region region){
-        return null; // TODO
+        ArrayList<Node> nodes = new ArrayList<>();
+
+        for(Map.Entry<Node, String> entry: database.getCurrentWork().entrySet()){
+            String[] work = entry.getValue().split(":");
+            String jobID = work[0];
+            String regionID = work[1];
+
+            if(jobID.equals(job.getId()) && regionID.startsWith(region.getId())){
+                nodes.add(entry.getKey());
+            }
+        }
+
+        return nodes;
     }
 
     public synchronized Collection<Node> getNodesForJobIDAndRegionID(String jobID, String regionID){
-        return null; // TODO
+        return getNodesForJobAndRegion(getJobFromID(jobID), new Region(regionID));
     }
 
     public synchronized Node getSuccessor(){
+        return getSuccessorForNode(database.getServent());
+    }
+
+    public synchronized Node getSuccessorForNode(Node node){
         return null; // TODO
     }
 
     public synchronized Node getPredecessor(){
+        return getPredecessorForNode(database.getServent());
+    }
+
+    public synchronized Node getPredecessorForNode(Node node){
         return null; // TODO
     }
 
@@ -65,23 +110,23 @@ public class DatabaseService {
     }
 
     public synchronized Node getInfo(){
-        return database.getServent(); // TODO
+        return database.getServent();
     }
 
     public synchronized void saveData(Point point){
-        // TODO
+        database.getMyData().add(point);
     }
 
     public synchronized List<Point> getCurrentData(){
-        return null; // TODO
+        return database.getMyData();
     }
 
     public synchronized Region getMyRegion(){
-        return null; // TODO
+        return database.getMyRegion();
     }
 
     public synchronized void saveNode(Node node){
-
+        database.getAllNodes().add(node);
     }
 
 
