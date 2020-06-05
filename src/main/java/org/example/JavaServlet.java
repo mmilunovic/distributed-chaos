82dfde5c37db7b6bc0ea519274ba95081cae2d28
Mihailo97 <mvignjevic97@gmail.com>
2020-06-05 02:23:09 +0200

    Polecemo

--- src/main/java/org/example/JavaServlet.java
+++ src/main/java/org/example/JavaServlet.java
@@ -39,7 +39,7 @@ public class JavaServlet {
         if(enteringNode.getAddress() == null){
             messageService.sendBootstrapNew();
         }else{
-            
+
         }
 
     }
--- src/main/java/org/example/service/UrlFactory.java
+++ src/main/java/org/example/service/UrlFactory.java
@@ -1,7 +1,20 @@
 package org.example.service;
 
+import org.example.config.ServentConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UrlFactory {
+
+    @Autowired
+    ServentConfig config;
+
+    public String getBootstrapHailUrl(){
+        return "http://" + config.getBootstrap() + "/api/bootstrap/hail";
+    }
+
+    public String getBootstrapNewUrl(){
+        return "http://" + config.getBootstrap() + "/api/bootstrap/new";
+    }
 }
